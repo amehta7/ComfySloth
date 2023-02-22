d4452fc65b5520e56c33ea66f20007e2af9fe769
amehta7 <amimehta2811@gmail.com>
2023-02-21 21:43:40 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ecart_App
+
+Check out our comfy sloth products for the very best in unique or custom, handmade pieces from our shops.
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import About from "./AboutPage";
+import Home from "./HomePage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  About,
+  Home,
+  Cart,
+  Error,
+  Products,
+  SingleProduct,
+  Checkout,
+  PrivateRoute,
+};
